6756a48a23939cb94df2cccb62ed1bd221175124
Samuel Andreo <samuanv@gmail.com>
2022-05-23 22:49:55 +0200

    Example: how to include interactors

--- src/core/interactors/index.ts
+++ src/core/interactors/index.ts
@@ -1,8 +1,15 @@
 import saveBooking from './saveBooking.interactor';
+import getOneById from './getOneById.interactor';
 import TripMongo from '../../dataSources/tripMongo.datasource';
 import EmailNotifier from '../../dataSources/emailNotifier.datasource';
 
 const tripRepository = new TripMongo();
 
 const notifierRepository = new  EmailNotifier();
-export default saveBooking(tripRepository, notifierRepository);
+
+
+export default {
+    saveBooking: saveBooking(tripRepository, notifierRepository),
+    // Pass repositories needed here
+    getOneById: getOneById()
+}
